ae718cf87e7e0cb7343807583e65399308851abe
Eliabner Teixeira <iamelitm@gmail.com>
2022-10-26 15:10:32 -0300

    feat: using deployer lib and setup deploy workflow action

---
+++ deploy.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+require 'contrib/npm.php';
+require 'contrib/rsync.php';
+
+///////////////////////////////////    
+// Config
+///////////////////////////////////
+
+set('application', 'PUC Pets');
+set('repository', 'https://fkw3t:' . env('GITHUB_REPO_TOKEN') .  '@github.com/fkw3t/puc-pets.git'); // Git Repository
+set('ssh_multiplexing', true);  // Speed up deployment
+//set('default_timeout', 1000);
+
+set('rsync_src', function () {
+    return __DIR__; // If your project isn't in the root, you'll need to change this.
+});
+
+// Configuring the rsync exclusions.
+// You'll want to exclude anything that you don't want on the production server.
+add('rsync', [
+    'exclude' => [
+        '.git',
+        '/vendor/',
+        '/node_modules/',
+        '.github',
+        'deploy.php',
+    ],
+]);
+
+// Set up a deployer task to copy secrets to the server.
+// Grabs the dotenv file from the github secret
+task('deploy:secrets', function () {
+    file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
+    upload('.env', get('deploy_path') . '/shared');
+});
+
+///////////////////////////////////
+// Hosts
+///////////////////////////////////
+
+host('prod') // Name of the server
+->setHostname('ec2-18-230-186-160.sa-east-1.compute.amazonaws.com') // Hostname or IP address
+->set('remote_user', 'ubuntu') // SSH user
+->set('branch', 'main') // Git branch
+->set('deploy_path', '/var/www/puc-pets'); // Deploy path
+
+after('deploy:failed', 'deploy:unlock');  // Unlock after failed deploy
+
+///////////////////////////////////
+// Tasks
+///////////////////////////////////
+
+desc('Start of Deploy the application');
+
+task('deploy', [
+    'deploy:prepare',
+    'rsync',                // Deploy code & built assets
+    'deploy:secrets',       // Deploy secrets
+    'deploy:vendors',
+    'deploy:shared',        // 
+    'artisan:storage:link', //
+    'artisan:view:cache',   //
+    'artisan:config:cache', // Laravel specific steps
+    'artisan:migrate',      //
+    'artisan:queue:restart',//
+    'deploy:publish',       // 
+]);
+
+desc('End of Deploy the application');
